2b747954abef34ee987f30982f1041e01c90826e
Aleksey <del-fisha@yandex.ru>
2023-01-25 22:50:45 +0300

    Done 2.9

---
+++ Python на примерах и задачах/2. Основные операции/9.py
@@ -0,0 +1,5 @@
+first = int(input("Введите первое число: "))
+second = int(input("Введите второе число "))
+
+result = "Второе число больше первого" if first < second else "Первое число больше второго"
+print(result)
