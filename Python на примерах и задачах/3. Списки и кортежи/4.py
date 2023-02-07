43f2fe90d3e09d3535c4c78b73351f6b931b9213
Aleksey <del-fisha@yandex.ru>
2023-02-07 12:14:33 +0300

    Done 3.4 Исправлены синтаксические ошибки

--- Python на примерах и задачах/3. Списки и кортежи/4.py
+++ Python на примерах и задачах/3. Списки и кортежи/4.py
@@ -1,15 +1,12 @@
-import random
-
 size = int(input("Введите размер стороны матрицы: "))
 array = [[0 for i in range(size)] for j in range(size)]
-
-def snake(list):
+def snake(arr):
     s = 1
     x = 0
     y = -1
     d_row = 1
     d_column = 0
-    lenght_of_elem = len(str(size ** 2))
+    length = len(str(size ** 2))
     while s <= size ** 2:
         if 0 <= x + d_row < size and 0 <= y + d_column < size and array[x + d_row][y + d_column] == 0:
             x += d_row
@@ -30,9 +27,9 @@ def snake(list):
                 d_row = 1
                 d_column = 0
 
-    for i in list:
+    for i in arr:
         for j in i:
-            print(str(j).rjust(lenght_of_elem), end=" ")
+            print(str(j).rjust(length), end=" ")
         print()
 
 
